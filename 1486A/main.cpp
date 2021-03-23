74f22d6545fa8c0c4c3199d49a429b83706c132c
besforyou <besforyou2@gmail.com>
2021-03-22 20:23:51 -0700

    21.03.23

---
+++ 1486A/main.cpp
@@ -0,0 +1,28 @@
+#include <iostream>
+#include <vector>
+     
+using namespace std;
+     
+void solve() {
+  int n;
+  cin >> n;
+  vector<int> a(n);
+  for (auto &i : a) cin >> i;
+  long long sum = 0, need = 0;
+  for (int i = 0; i < n; ++i) {
+   	need += i;
+  	sum += a[i];
+  	if (sum < need) {
+      cout << "NO\n";
+      return;
+    }
+  }
+  cout << "YES\n";
+}
+     
+int main() {
+  int t;
+  cin >> t;
+  while (t--) solve();
+  return 0;
+}
