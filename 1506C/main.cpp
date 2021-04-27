3460751ad86a9f19e91f9c34868f87fba8fbc509
besforyou <besforyou2@gmail.com>
2021-04-27 07:23:51 -0700

    21.04.27

---
+++ 1506C/main.cpp
@@ -0,0 +1,33 @@
+#include <iostream>
+
+using namespace std;
+
+int main() {
+
+	int t;
+	cin >> t;
+
+	while (t--) {
+
+		string a, b;
+		cin >> a >> b;
+
+		int n = a.size() , m = b.size();
+		int ans = 0;
+
+		for (int len = 1; len <= min(n,m) ; len++ ) {
+			for ( int i = 0 ; i + len <= n ; i++ ) {
+				for ( int j = 0; j + len <= m ; j++ ) {
+					if ( a.substr(i,len) == b.substr(j,len) )
+						ans = max(ans, len);					
+				}
+			}
+		}
+
+		cout << n - ans + m - ans << endl;
+	}
+
+}
+		
+
+		
